be729461fe92049025833f37db74057b17201a27
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-06-27 19:08:00 -0400

    Added login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,7 +13,7 @@ class UserController < ApplicationController
       redirect session[:sign_up_referrer]
 
     elsif User.find_by(email: params[:email])
-      create_message("you already have an account. If you forgot it you can request a paasword reset", "notice")
+      create_message("you already have an account. If you forgot your password, you can request a paasword reset", "notice")
       redirect "/account/login"
     else
       
@@ -26,6 +26,22 @@ class UserController < ApplicationController
     session[:authenticate_referrer] = request.referrer
     # user.authenticate(password) # to sign_in
     erb :'users/login'
+  end
+
+  post "/account/login" do
+    user.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      create_message("welcome #{user.full_name}", "successful")
+      redirect session[:sign_up_referrer]
+
+    elsif User.find_by(email: params[:email])
+      create_message("you already have an account. If you forgot your password, you can request a paasword reset", "notice")
+      redirect "/account/login"
+      
+    else
+      create_message("thats not your password, try again", "failure")
+      redirect "/account/login"
+    end
   end
   
 
