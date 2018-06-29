a7a18a3e6d41b1f11acc7bcd3a89c6e903b1f2a1
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-06-29 12:49:54 -0400

    Created all 6 remaining database tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625164339) do
+ActiveRecord::Schema.define(version: 20180629164326) do
+
+  create_table "User_track_progress", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "track_id"
+    t.integer  "last_comleted_chapter_id"
+    t.integer  "last_comleted_quiz_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.string   "content"
+    t.boolean  "is_correct"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "chapters", force: :cascade do |t|
+    t.integer  "track_id"
+    t.string   "title"
+    t.integer  "sort_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "quiz_id"
+    t.string   "title"
+    t.string   "type"
+    t.integer  "sort_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.integer  "chapter_id"
+    t.string   "title"
+    t.integer  "sort_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.integer  "owner_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
