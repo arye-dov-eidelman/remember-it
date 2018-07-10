4bdc4119d3e0a5be04130b2cde659a6c47cef703
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-07-10 13:23:44 -0400

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :first_name,  presence: true
   validates :last_name,  presence: true
-  validates :email,     presence: true, uniqueness: true
+  validates :email,     presence: true, uniqueness: true, email: true
   validates :password,  presence: true
 
   # To Do: rename user_track_progress to somthing better!
