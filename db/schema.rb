4b10b8a044d75ea470bbad20c8d63cbbf1b2c2af
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-07-15 21:45:17 -0400

    switched to postgress for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180629164326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
     t.string   "content"
